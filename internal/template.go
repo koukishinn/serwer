cb39787b8c724d9403108422eba470acff641f59
Rafael Campos Nunes <rcamposnunes@outlook.com>
2024-12-22 23:10:58 -0300

    Move and improve existing templates
    Signed-off-by: Rafael Campos Nunes <rcamposnunes@outlook.com>

--- internal/template.go
+++ internal/template.go
@@ -23,5 +23,21 @@ var (
 		"join": func(s string, p string) string {
 			return filepath.Join(s, p)
 		},
+		"truncate": func(n float64) float64 {
+			mask := int64(n*100) & 0xFFF
+
+			return float64(mask) / 100
+		},
 	}
 )
+
+type TemplatePath string
+
+const (
+	TemplateFiles   TemplatePath = "www/templates/files.tmpl"
+	TemplatePreview TemplatePath = "www/templates/preview.tmpl"
+)
+
+func (t TemplatePath) Path() string {
+	return string(t)
+}
