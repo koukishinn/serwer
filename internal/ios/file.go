e310b24ceb9b7da62b4ea953e44feb5f6085f923
Rafael Campos Nunes <rcamposnunes@outlook.com>
2024-12-22 23:09:58 -0300

    Add a method to compute file size
    Signed-off-by: Rafael Campos Nunes <rcamposnunes@outlook.com>

--- internal/ios/file.go
+++ internal/ios/file.go
@@ -6,11 +6,22 @@ import (
 )
 
 type File struct {
-	Name  string
-	IsDir bool
-	Size  int64
-	Path  string
+	Name      string
+	IsDir     bool
+	Size      float64
+	SizeOrder string
+	Path      string
 }
+
+var (
+	orders = map[float64]string{
+		1 << 0:  "B",
+		1 << 10: "KB",
+		1 << 20: "MB",
+		1 << 30: "GB",
+		1 << 40: "TB",
+	}
+)
 
 // Read a directory, giving back a list of files and subdirectories
 func Read(dir string) ([]File, error) {
@@ -23,13 +34,33 @@ func Read(dir string) ([]File, error) {
 
 	for _, file := range entry {
 		info, _ := file.Info()
+
+		size, order := compute(info.Size())
+
 		files = append(files, File{
-			Name:  file.Name(),
-			IsDir: file.IsDir(),
-			Size:  info.Size(),
-			Path:  filepath.Join(dir, file.Name()),
+			Name:      file.Name(),
+			IsDir:     file.IsDir(),
+			Size:      size,
+			SizeOrder: order,
+			Path:      filepath.Join(dir, file.Name()),
 		})
 	}
 
 	return files, err
+}
+
+func compute(i int64) (float64, string) {
+	original := float64(i)
+
+	size := original
+	order := orders[1]
+
+	for k, v := range orders {
+		if original/k >= 1 {
+			size = original / k
+			order = v
+		}
+	}
+
+	return size, order
 }
--- internal/ios/file_test.go
+++ internal/ios/file_test.go
@@ -5,6 +5,28 @@ import (
 	"testing"
 )
 
+func TestCompute(t *testing.T) {
+	tests := []struct {
+		Size         int64
+		ExpectedSize int64
+		Order        string
+	}{
+		{Size: 4096, Order: "KB", ExpectedSize: 4},
+		{Size: 8192, Order: "KB", ExpectedSize: 8},
+	}
+
+	for _, test := range tests {
+		size, order := compute(test.Size)
+
+		if order != test.Order {
+			t.Errorf("want %s order, got %s", test.Order, order)
+		}
+		if int64(size) != test.ExpectedSize {
+			t.Errorf("wrong expected size, want %d, got %f", test.ExpectedSize, size)
+		}
+	}
+}
+
 func TestFile(t *testing.T) {
 	directory, err := os.MkdirTemp(os.TempDir(), "server")
 	if err != nil {
@@ -29,7 +51,7 @@ func TestFile(t *testing.T) {
 	}
 
 	for _, file := range infos {
-		t.Log(file.Name())
+		t.Log(file.Name)
 	}
 
 	t.Cleanup(func() {
