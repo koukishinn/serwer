31477151d3f9289588bb15d4750d8b73593fe3d7
Rafael Campos Nunes <rcamposnunes@outlook.com>
2025-01-18 14:25:40 -0300

    Make functions more testable and fix truncate
    Signed-off-by: Rafael Campos Nunes <rcamposnunes@outlook.com>

--- internal/template.go
+++ internal/template.go
@@ -2,34 +2,19 @@ package internal
 
 import (
 	"html/template"
+	"math"
 	"path/filepath"
 	"strings"
 )
 
 var (
 	TemplateFunctions = template.FuncMap{
-		"add": func(a, b int) int { return a + b },
-		"sub": func(a, b int) int { return a - b },
-		"until": func(n int) []int {
-			var result []int
-			for i := 0; i < n; i++ {
-				result = append(result, i)
-			}
-			return result
-		},
-		"split": func(s string, p string) []string {
-			return strings.Split(s, p)
-		},
-		"join": func(s string, p string) string {
-			return filepath.Join(s, p)
-		},
-		"truncate": func(n float64) float64 {
-			// FIXME: I have to figure out how to get the exponent to know how I should
-			// divide the final number
-			mask := int64(n*100) & 0xFFF
-
-			return float64(mask)
-		},
+		"add":      add,
+		"sub":      sub,
+		"until":    until,
+		"split":    split,
+		"join":     join,
+		"truncate": truncate,
 	}
 )
 
@@ -43,3 +28,28 @@ const (
 func (t TemplatePath) Path() string {
 	return string(t)
 }
+
+var (
+	add = func(a, b int) int { return a + b }
+	sub = func(a, b int) int { return a - b }
+
+	until = func(n int) []int {
+		var result []int
+		for i := 0; i < n; i++ {
+			result = append(result, i)
+		}
+		return result
+	}
+
+	split = func(s string, p string) []string {
+		return strings.Split(s, p)
+	}
+
+	join = func(s string, p string) string {
+		return filepath.Join(s, p)
+	}
+
+	truncate = func(n float64) float64 {
+		return float64(int(math.Floor(n*1000)) / 1000)
+	}
+)
